c79edf868a69f8ef75eb9ff495983bb47b20ba1a
Qayyax <qayyax@gmail.com>
2022-06-30 09:41:23 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if successful
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
