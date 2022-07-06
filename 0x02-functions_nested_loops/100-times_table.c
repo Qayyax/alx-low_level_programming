d8ef614d5662785f95d1b0d1707f16c6d21f644b
Qayyax <qayyax@gmail.com>
2022-07-05 19:21:36 -0700

    finalcorrection

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,57 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print - function to print recursively using _putchar
+ * @var: variable to be printed
  */
+void print(int var)
+{
+	if (var / 10)
+		print(var / 10);
+	_putchar('0' + (var % 10));
+}
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: time table to print
+ *
+ * Created by - Qayyax
+ * cc; 5th july, 2022
+ */
+
 void print_times_table(int n)
 {
-	int i, j, k;
+	int ii, jj, mul;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+	for (ii = 0; ii <= n; ii++)
 	{
-		for (i = 0; i <= n; i++)
+		for (jj = 0; jj <= n; jj++)
 		{
-			for (j = 0; j <= n; j++)
+			mul = ii * jj;
+			if (jj == 0)
+				_putchar('0' + mul);
+			else if (mul < 10)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + mul);
 			}
-			_putchar('\n');
+			else if (mul < 100)
+			{
+				_putchar(' ');
+				print(mul);
+			}
+				else
+			{
+				print(mul);
+			}
+			if (jj < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
