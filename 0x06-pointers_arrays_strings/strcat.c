b9fbc75c9d8af471bb5bacf91bb390d622505700
Qayyax <qayyax@gmail.com>
2022-07-14 10:29:48 -0700

    yay

--- 0x06-pointers_arrays_strings/strcat.c
+++ 0x06-pointers_arrays_strings/strcat.c
@@ -1,22 +1,27 @@
+#include "main.h"
 
-       #include <string.h>
-       #include <time.h>
-       #include <stdio.h>
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destLen = 0;
+	int srcLen = 0;
 
-       int
-       main(int argc, char *argv[])
-       {
-       #define LIM 4000000
-           int j;
-           char p[LIM + 1];    /* +1 for terminating null byte */
-           time_t base;
+	while (dest[destLen] != '\0')
+		destLen++;
 
-           base = time(NULL);
-           p[0] = '\0';
+	while (src[srcLen])
+	{
+		dest[destLen] = src[srcLen];
+		srcLen++;
+		destLen++;
+	}
+	dest[destLen] = '\0';
 
-           for (j = 0; j < LIM; j++) {
-               if ((j % 10000) == 0)
-                   printf("%d %ld\n", j, (long) (time(NULL) - base));
-               strcat(p, "a");
-           }
-       }
+	return (dest);
+}
