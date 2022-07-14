2c0899bb105d22d1c71276e0fc3879295818db12
Qayyax <qayyax@gmail.com>
2022-07-14 10:03:35 -0700

    fix

--- 0x06-pointers_arrays_strings/strcat.c
+++ 0x06-pointers_arrays_strings/strcat.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-int main() {
-   char str1[100] = "This is ", str2[] = "programiz.com";
 
-   // concatenates str1 and str2
-   // the resultant string is stored in str1.
-   strcat(str1, str2);
+       #include <string.h>
+       #include <time.h>
+       #include <stdio.h>
 
-   puts(str1);
-   puts(str2);
+       int
+       main(int argc, char *argv[])
+       {
+       #define LIM 4000000
+           int j;
+           char p[LIM + 1];    /* +1 for terminating null byte */
+           time_t base;
 
-   return 0;
-}
+           base = time(NULL);
+           p[0] = '\0';
+
+           for (j = 0; j < LIM; j++) {
+               if ((j % 10000) == 0)
+                   printf("%d %ld\n", j, (long) (time(NULL) - base));
+               strcat(p, "a");
+           }
+       }
